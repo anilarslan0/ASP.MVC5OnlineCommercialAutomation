1821033254fbce2c4d111eaa3a82c295910e736b
Anıl ARSLAN <anlarsln0@gmail.com>
2021-02-23 03:16:07 +0300

    Category CRUD Added

--- MvcOnlineTicariOtomasyon/Controllers/CategoryController.cs
+++ MvcOnlineTicariOtomasyon/Controllers/CategoryController.cs
@@ -3,15 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcOnlineTicariOtomasyon.Models.Class;
 
 namespace MvcOnlineTicariOtomasyon.Controllers
 {
     public class CategoryController : Controller
     {
         // GET: Category
+
+        Context context = new Context();
         public ActionResult Index()
         {
+            var degerler = context.Categories.ToList();
+            return View(degerler);
+        }
+
+        [HttpGet]
+        public ActionResult KategoriEkle()
+        {
             return View();
+        }
+        [HttpPost]
+        public ActionResult KategoriEkle(Category category)
+        {
+            context.Categories.Add(category);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+            
+        }
+
+        public ActionResult KategoriSil(int id)
+        {
+            var kategori = context.Categories.Find(id);
+            context.Categories.Remove(kategori);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult KategoriGetir(int id)
+        {
+            var kategori = context.Categories.Find(id);
+            return View("KategoriGetir", kategori)
+;        }
+
+        public ActionResult KategoriGuncelle(Category category)
+        {
+            var kategori = context.Categories.Find(category.CategoryId);
+            kategori.CategoryName = category.CategoryName;
+            context.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
