fd913a68e777dbd1e71eb550812ce7daf7e3a363
Anıl ARSLAN <anlarsln0@gmail.com>
2021-04-14 01:22:53 +0300

    commit

---
+++ MvcOnlineTicariOtomasyon/Controllers/GraphicController.cs
@@ -0,0 +1,69 @@
+﻿using MvcOnlineTicariOtomasyon.Models.Class;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class GraphicController : Controller
+    {
+        // GET: Graphic
+        Context context = new Context();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Grafik()
+        {
+            var graphicDraw = new Chart(600,600);
+            graphicDraw.AddTitle("Kategori - Ürün Stok Sayısı").AddLegend("Stok").AddSeries("Değerler", xValue: new[] { "Mobilya", "Ofis Eşyaları", "Bilgisayar" },
+                yValues: new[] { 85, 66, 98 }).Write();
+            return File(graphicDraw.ToWebImage().GetBytes(), "image/jpeg");
+
+        }
+
+        public ActionResult Index3()
+        {
+            ArrayList xvalue = new ArrayList();
+            ArrayList yvalue = new ArrayList();
+            var sonuclar = context.Products.ToList();
+            sonuclar.ToList().ForEach(x => xvalue.Add(x.ProductName));
+            sonuclar.ToList().ForEach(y=>yvalue.Add(y.ProductStock));
+            var grafic = new Chart(width: 800, height: 800).AddTitle("Stoklar").AddSeries(chartType: "Pie", name: "Stok", xValue: xvalue, yValues: yvalue);
+            return File(grafic.ToWebImage().GetBytes(), "image/jpeg");
+
+        }
+
+        public ActionResult Index5()
+        {
+            return View();
+        }
+
+        public ActionResult VisualizeProductResult()
+        {
+            return Json(ProductList(),JsonRequestBehavior.AllowGet);
+        }
+
+        public List<ChartFor> ProductList()
+        {
+            List<ChartFor> chart = new List<ChartFor>();
+            using (var context=new Context())
+            {
+                chart = context.Products.Select(x => new ChartFor
+                {
+                    ProductName = x.ProductName,
+                    Stok = x.ProductStock
+                }).ToList();
+            }
+            return chart;
+        }
+
+
+
+    }
+}
---
+++ MvcOnlineTicariOtomasyon/Models/Class/ChartFor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcOnlineTicariOtomasyon.Models.Class
+{
+    public class ChartFor
+    {
+        public string ProductName { get; set; }
+        public short Stok { get; set; }
+    }
+}
